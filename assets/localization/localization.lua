c889f0cc8b9ad285057726e87b5f7ad9fec8a94b
d954mas <d954mas@gmail.com>
2021-04-14 16:19:49 +0300

    add localization

--- assets/localization/localization.lua
+++ assets/localization/localization.lua
@@ -8,7 +8,25 @@ local FALLBACK = DEFAULT
 
 ---@class Localization
 local M = {
-	btnPlay = { en = "Play", ru = "Играть" },
+	main_title = { en = "SPACE\nDEFENDER", ru = "КОСМИЧЕСКИЙ\nЗАЩИТНИК" },
+	btn_play = { en = "PLAY", ru = "ИГРАТЬ" },
+	btn_menu = { en = "MENU", ru = "МЕНЮ" },
+	btn_highscore = { en = "HIGHSCORE", ru = "РЕКОРДЫ" },
+	btn_shop = { en = "SHOP", ru = "МАГАЗИН" },
+	btn_restart = { en = "RESTART", ru = "ПЕРЕЗАПУСК" },
+	btn_money_x2 = { en = "money X2 (ads)", ru = "деньги X2 (реклама)" },
+
+	highscore_title = { en = "HIGHSCORE", ru = "РЕКОРДЫ" },
+
+	shop_title = { en = "SHOP", ru = "МАГАЗИН" },
+	shop_money_lbl = { en = "%{money}$", ru = "%{money}$" },
+	shop_firerate_lbl = { en = "Increase fire rate:%{money}", ru = "Cкорострельность:%{money}$" },
+
+	lose_title = { en = "YOU LOSE", ru = "ПРОИГРЫШ" },
+	lose_score_lbl = { en = "Score:%{score}", ru = "Очки:%{score}" },
+	lose_money_lbl = { en = "Money:%{money}", ru = "Деньги:%{money}$" },
+	lose_wave_lbl = { en = "Wave:%{wave}", ru = "Волна:%{wave}" },
+
 }
 
 function M:locale_exist(key)
@@ -21,6 +39,10 @@ end
 function M:set_locale(locale)
 	LOG.w("set locale:" .. locale,TAG)
 	I18N.setLocale(locale)
+end
+
+function M:locale_get()
+	return I18N.getLocale()
 end
 
 I18N.setFallbackLocale(FALLBACK)
@@ -50,6 +72,7 @@ for k, v in pairs(M) do
 end
 
 --return key if value not founded
+---@type Localization
 local t = setmetatable({ __VALUE = M, }, {
 	__index = function(_, k)
 		local result = M[k]
