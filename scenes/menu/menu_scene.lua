2db236a3df6d9a5adca952dc5c1a4fc62c890ba3
d954mas <d954mas@gmail.com>
2021-04-14 10:45:44 +0300

    fixed game size

--- scenes/menu/menu_scene.lua
+++ scenes/menu/menu_scene.lua
@@ -1,5 +1,3 @@
-local WORLD = require "world.world"
-
 local BaseScene = require "libs.sm.scene"
 
 
@@ -8,15 +6,16 @@ local Scene = BaseScene:subclass("Menu")
 function Scene:initialize()
     BaseScene.initialize(self, "MenuScene", "/menu_scene#collectionproxy")
     self._config.keep_loaded = true
+    self.world = reqf "world.world"
 end
 
 function Scene:show_done()
     BaseScene.show_done(self)
-    WORLD.ads:banner_show("menu")
+    self.world.ads:banner_show("menu")
 end
 
 function Scene:hide_done()
-    WORLD.ads:banner_hide("menu")
+    self.world.ads:banner_hide("menu")
 end
 
 return Scene
